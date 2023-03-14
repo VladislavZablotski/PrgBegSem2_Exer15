2e0ff7822290de06a6ba807b98eba248e980d1ef
unknown <zab_vl@tutby>
2023-03-14 08:50:21 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string[] days = {"рабочим","рабочим","рабочим","рабочим","рабочим","выходным","выходным"};
+Console.Write("Введите день недели числом: ");
+int date = Convert.ToInt32(Console.ReadLine());
+if(date<=7&&date>0)
+{
+    Console.Write($"Данный день является {days[date-1]}");
+}
+else
+{
+    Console.WriteLine("Неверно задан день!");
+}
